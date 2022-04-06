28d2bb54d841330f6ea5d8d557d771289b17b184
AlexMoree <alex.d.moree@gmail.com>
2022-04-06 15:47:49 -0500

    Made all py functions for Users

--- my-react-app/src/botBackend.py
+++ my-react-app/src/botBackend.py
@@ -2,7 +2,6 @@
 import psycopg2
 import subprocess
 
-
 def LoginValidation(Input1, Input2):
     HEROKU_APP_NAME = "botproject-csce315"
     import subprocess, psycopg2
@@ -13,18 +12,78 @@ def LoginValidation(Input1, Input2):
     cursor = conn.cursor()
     n = ""
     cursor.execute("SELECT * FROM Users WHERE Username = " + "\'"+ Input1 + "\'" + " AND Password = " + "\'"+ Input2 + "\'" + ";")
-    n = cursor.fetchone();
+    n = cursor.fetchone()
+    cursor.close()
+    conn.close()
     #print(n)
     if type(n) != type(None):
     	print ("Access Granted:" + Input1)
     	return True
     else:
-    	print("Access Denied: Try again")
-    #conn.commit()
-    cursor.close()
-    conn.close()
+    	print("Incorrect Infor")
+        return False
 
 inputUser = input("Enter username: ")
 inputPass = input("Enter password: ")
 
 LoginValidation(inputUser, inputPass)
+
+############################### NEEDS TO BE TESTES ##############################
+def EnterUserToTable(Username, Password):
+    HEROKU_APP_NAME = "botproject-csce315"
+    import subprocess, psycopg2
+    # connection and execution
+    conn_info = subprocess.run(["heroku", "config:get", "DATABASE_URL", "-a", HEROKU_APP_NAME], stdout = subprocess.PIPE)
+    connuri = conn_info.stdout.decode('utf-8').strip()
+    conn = psycopg2.connect(connuri)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Users VALUES('",Username,"','",Password,"','Null','Null');")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Updated Users Table with -", Username)
+
+############################### NEEDS TO BE TESTES ##############################
+def RemoveUserFromTable(Username):
+    HEROKU_APP_NAME = "botproject-csce315"
+    import subprocess, psycopg2
+    # connection and execution
+    conn_info = subprocess.run(["heroku", "config:get", "DATABASE_URL", "-a", HEROKU_APP_NAME], stdout = subprocess.PIPE)
+    connuri = conn_info.stdout.decode('utf-8').strip()
+    conn = psycopg2.connect(connuri)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROME Users WHERE Username = '",Username,"');")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Deleted -", Username, "- From the table Users")   
+
+############################### NEEDS TO BE TESTES ##############################
+def UpdateEmail(Username, Email):
+    HEROKU_APP_NAME = "botproject-csce315"
+    import subprocess, psycopg2
+    # connection and execution
+    conn_info = subprocess.run(["heroku", "config:get", "DATABASE_URL", "-a", HEROKU_APP_NAME], stdout = subprocess.PIPE)
+    connuri = conn_info.stdout.decode('utf-8').strip()
+    conn = psycopg2.connect(connuri)
+    cursor = conn.cursor()
+    cursor.execute("UPDATE Users SET Email = '",Email,"' WHERE Username = '",Username,"';")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Updated User -", Username, " - with Email -", Email)
+
+############################### NEEDS TO BE TESTES ##############################
+def UpdatePhone(Username, Phone):
+    HEROKU_APP_NAME = "botproject-csce315"
+    import subprocess, psycopg2
+    # connection and execution
+    conn_info = subprocess.run(["heroku", "config:get", "DATABASE_URL", "-a", HEROKU_APP_NAME], stdout = subprocess.PIPE)
+    connuri = conn_info.stdout.decode('utf-8').strip()
+    conn = psycopg2.connect(connuri)
+    cursor = conn.cursor()
+    cursor.execute("UPDATE Users SET Phone = '",Phone,"' WHERE Username = '",Username,"';")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Updated User -", Username, " - with Phone -", Phone)
