43bdccf7fc46170e3b269ab5f8baa4d312c4fb6b
Daviddk226 <Daviddk226@gmail.com>
2022-05-02 02:22:12 -0500

    Setup scheduling to host on heroku

---
+++ my-react-app/clock.py
@@ -0,0 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from main import *
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=60)
+def timed_job():
+    main_call_frame()
+    #print("Running scheduled job")
+
+sched.start()
